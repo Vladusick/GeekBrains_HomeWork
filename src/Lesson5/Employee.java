d331a33d5567831b4b744513b100a800f90d3bb5
Vladusick <samuelfs@mail.ru>
2021-10-19 13:04:28 +0400

    HomeWork 5

---
+++ src/Lesson5/Employee.java
@@ -0,0 +1,37 @@
+package Lesson5;
+
+public class Employee {
+
+    private String fullName;
+    private String positon;
+    private String email;
+    private int telephone;
+    private int theSalary;
+    public int age;
+
+    public Employee(String fullName, String positon, String email, int telephone, int theSalary, int age) {
+        this.fullName = fullName;
+        this.positon = positon;
+        this.email = email;
+        this.telephone = telephone;
+        this.theSalary = theSalary;
+        this.age = age;
+
+        }
+
+public void printInfo () {
+
+    System.out.println("fullName: " + fullName + " positon: " + positon + " email: " + email + " telephone: "
+            + telephone + " theSalary: " + theSalary + " age: " + age );
+
+}
+
+
+
+    }
+
+
+
+
+
+
