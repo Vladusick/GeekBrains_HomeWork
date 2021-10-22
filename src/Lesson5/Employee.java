724f9e9ba8855ec7a69ec3dbc467cd4a148af589
Vladusick <samuelfs@mail.ru>
2021-10-22 12:22:49 +0400

    HomeWork 5.1

--- src/Lesson5/Employee.java
+++ src/Lesson5/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
     private String email;
     private int telephone;
     private int theSalary;
-    public int age;
+    protected int age;
 
     public Employee(String fullName, String positon, String email, int telephone, int theSalary, int age) {
         this.fullName = fullName;
@@ -17,18 +17,21 @@ public class Employee {
         this.theSalary = theSalary;
         this.age = age;
 
-        }
+    }
 
-public void printInfo () {
+    public int getAge() {
+        return age;
+    }
 
-    System.out.println("fullName: " + fullName + " positon: " + positon + " email: " + email + " telephone: "
-            + telephone + " theSalary: " + theSalary + " age: " + age );
+    public void printInfo() {
 
-}
-
-
+        System.out.println("fullName: " + fullName + " positon: " + positon + " email: " + email + " telephone: "
+                + telephone + " theSalary: " + theSalary + " age: " + age);
 
     }
+
+
+}
 
 
 
--- src/Lesson5/TestEmployee.java
+++ src/Lesson5/TestEmployee.java
@@ -5,22 +5,22 @@ public class TestEmployee {
 
     public static void main(String[] args) {
 
-       // Employee p1 = new Employee("Vlad", "manager", "sdfsdfsdf", 498494, 464984, 41);
+        // Employee p1 = new Employee("Vlad", "manager", "sdfsdfsdf", 498494, 464984, 41);
 
         Employee[] empArray = new Employee[5];
         empArray[0] = new Employee("Petr Ivanov", "manager", "petr@mail.ru",
                 905471211, 123000, 25);
         empArray[1] = new Employee("Kostya Vaschenko", "PR-manager", "Kostyapalka23@mail.ru",
-                905457132,130000,35);
+                905457132, 130000, 35);
         empArray[2] = new Employee("Denis Zagrendinov", "worker", "lyapa1992@mail.ru",
-                999157324,70000,29);
+                999157324, 70000, 29);
         empArray[3] = new Employee("Vadim Golovach", "teacher", "wood82@mail.ru",
-                987514252,750000,45);
+                987514252, 750000, 45);
         empArray[4] = new Employee("Dmitrii Batalov", "mechanic", "half-life2@bk.ru",
-                907547421,80000,41);
+                907547421, 80000, 41);
 
         for (int i = 0; i < empArray.length; i++) {
-            if (empArray[i].age > 40) empArray[i].printInfo();
+            if (empArray[i].getAge() > 40) empArray[i].printInfo();
         }
 
 
