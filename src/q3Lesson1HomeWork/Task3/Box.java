a5b83efa2b8f61f2f6446f52e1b943f08e05e35d
Vladusick <samuelfs@mail.ru>
2022-03-07 17:41:06 +0400

    Java 3 Lesson 1 HomeWork

---
+++ src/q3Lesson1HomeWork/Task3/Box.java
@@ -0,0 +1,41 @@
+package q3Lesson1HomeWork.Task3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Box<E extends Fruit> {
+
+    private List<E> fruits = new ArrayList<>();
+
+    public Box(List<E> fruits) {
+        this.fruits.addAll(fruits);
+    }
+
+    public Box(E... fruits) {
+        this.fruits.addAll(Arrays.asList(fruits));
+    }
+
+    public void add(E fruit) {
+        fruits.add(fruit);
+    }
+
+    public void moveAllTo(Box<E> anotherBox) {
+        for (E fruit : fruits) {
+            anotherBox.add(fruit);
+        }
+    fruits.clear();
+    }
+
+    public boolean compareTo(Box<?> anotherBox) {
+        return Math.abs(this.getWeight() - anotherBox.getWeight()) < 0.0001;
+    }
+
+    public double getWeight() {
+        double sum = 0.0;
+        for (E fruit : fruits) {
+            sum += fruit.getWeight();
+        }
+        return sum;
+    }
+}
