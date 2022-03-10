3fe787f562d8f956d8966d53e5ca7dbc610225da
Vladusick <samuelfs@mail.ru>
2022-03-10 16:26:14 +0400

    Java 3 Lesson 2 HomeWork

--- NetworkChatServer/src/main/java/ru/samoshchenko/server/chat/auth/AuthService.java
+++ NetworkChatServer/src/main/java/ru/samoshchenko/server/chat/auth/AuthService.java
@@ -2,13 +2,14 @@ package ru.samoshchenko.server.chat.auth;
 
 import java.util.Set;
 
-public class AuthService {
+public class AuthService implements IAuthService {
+
 
     private static Set<User> USERS = Set.of(
             new User("login1", "pass1", "username1"),
             new User("login2", "pass2", "username2"),
             new User("login3", "pass3", "username3")
-            );
+    );
 
     public String getUserNameByLoginAndPassword(String login, String password) {
         User requiredUser = new User(login, password);
@@ -19,4 +20,23 @@ public class AuthService {
         }
         return null;
     }
+
+    @Override
+    public void updateUsername(String currentUsername, String newUsername) {
+        User user = getUserByUsername(currentUsername);
+        if (user != null) {
+            user.setUsername(newUsername);
+        }
+    }
+
+    private User getUserByUsername(String username) {
+        for (User user : USERS) {
+            if (user.getUserName().equals(username)) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
 }
---
+++ NetworkChatServer/src/main/java/ru/samoshchenko/server/chat/auth/IAuthService.java
@@ -0,0 +1,16 @@
+package ru.samoshchenko.server.chat.auth;
+
+public interface IAuthService {
+
+    default void start() {
+
+    };
+
+    String getUserNameByLoginAndPassword(String login, String password);
+
+    default void stop() {
+
+    }
+
+    void updateUsername(String currentUsername, String newUsername);
+}
